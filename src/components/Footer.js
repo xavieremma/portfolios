5a041b3ae697e4b29acd0b09af5a98a1fa1c0a34
Xavier Emma <94151865+xavieremma@users.noreply.github.com>
2023-12-11 17:24:27 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/xavierremma"
+                href="https://www.instagram.com/xavierrremma"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
